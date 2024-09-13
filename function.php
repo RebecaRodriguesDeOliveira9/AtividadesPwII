4ffdf6d7315b4eb0c4a7f7a12535c5bd009c2ee6
rebeca <rebecarodriguesolver@gmail.com>
2024-09-12 22:32:44 -0300

    Peixe

---
+++ function.php
@@ -0,0 +1,10 @@
+funções nativas são funções que os próprios php 
+todos fonrce, como por exemplo:
+    a função isset(), //Verifica se existe a variavel
+    a função empty(), //Verifica se a variavel está vazia
+    a função count(), //Conta quantos itens tem no array
+    a função include "", //includeum arquivo dentro do outros
+    a função echo "", //Imprime na tela 
+    a função strel(), //Conta quantos caracteres tem uma string 
+    a função explode(), //Cria um array a partir de uma frase
+    a função replace(), // Troca caracteres dentro
